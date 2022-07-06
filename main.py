bbb413596e2958df80c5cd11220738cbed1d8353
MeggyMendes <meggymendes@hotmail.com>
2022-07-05 21:56:33 -0300

    create: main created

---
+++ main.py
@@ -0,0 +1,25 @@
+#exercicio_01
+#Programa em Python que solicite as informações de três pessoas, como nome, idade, peso, altura. 
+#Apresenta na tela estas informações de modo que permaneçam alinhados verticalmente com formatação de interpolação.
+
+print("Pessoa 1")
+var_nome1 = input('Digite seu nome: ')
+var_idade1 = input('Digite sua idade: ')
+var_peso1 = input('Digite seu peso: ')
+var_altura1 = input('Digite sua altura: ')
+
+print("Pessoa 2")
+var_nome2 = input('Digite seu nome: ')
+var_idade2 = input('Digite sua idade: ')
+var_peso2 = input('Digite seu peso: ')
+var_altura2 = input('Digite sua altura: ')
+
+print("Pessoa 3")
+var_nome3 = input('Digite seu nome: ')
+var_idade3 = input('Digite sua idade: ')
+var_peso3 = input('Digite seu peso: ')
+var_altura3 = input('Digite sua altura: ')
+
+print(f'Nome: {var_nome1}, Idade: {var_idade1}, Peso: {var_peso1}, Altura: {var_altura1}')
+print(f'Nome: {var_nome2}, Idade: {var_idade2}, Peso: {var_peso2}, Altura: {var_altura2}')
+print(f'Nome: {var_nome3}, Idade: {var_idade3}, Peso: {var_peso3}, Altura: {var_altura3}')
